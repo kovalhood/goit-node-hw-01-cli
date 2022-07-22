21d55464f0acf31eb71f0c9236e789f68e97dd3a
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-07-22 23:33:47 +0300

    Finished HW

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
-const users = require('./users');
-console.log(users);
-const { moders } = require('./users')
-console.log(moders);
+const contacts = require('./db/contacts');
+
+const { Command } = require('commander')
+const program = new Command()
+program
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone')
+
+program.parse(process.argv)
+
+const argv = program.opts()
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+    switch (action) {
+        case 'list':
+            const listContacts = await contacts.listContacts();
+            console.table(listContacts);
+            break;
+
+        case 'get':
+            const contactById = await contacts.getContactById(id);
+            console.log(contactById);
+            break;
+
+        case 'add':
+            const newContact = await contacts.addContact(name, email, phone);
+            console.log(newContact);
+            break;
+        
+        case 'update':
+            const updateContact = await contacts.updateContactById(id, name, email, phone);
+            console.log(updateContact);
+            break;
+
+        case 'remove':
+            const removeContact = await contacts.removeContact(id);
+            console.log(removeContact);
+            break;
+
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+    }
+}
+
+invokeAction(argv);
